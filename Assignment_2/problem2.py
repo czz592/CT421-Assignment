ed3bc5690c7cef7f43bf0121fa0815580b47d795
Leo Chui <z.chui1@universityofgalway.ie>
2024-03-15 20:51:52 +0000

    Update problem2 with relevant code
    TODO:
    
    - update plotting
    - adapt colour changing from problem1 for personality

--- Assignment_2/problem2.py
+++ Assignment_2/problem2.py
@@ -54,11 +54,10 @@ class Node():
         self.colour = None
         self.known_colours = []
         self.neighbours = []
-        
+
         # randomly have self.personality either be aggressive or passive
         self.personality = random.choice([0, 1])
-
-        self.strength = random.random() # personality strength
+        self.personality_strength = random.random()
 
     def communicate(self):
         """
@@ -69,16 +68,16 @@ class Node():
         Else move on
         """
         for neighbour in self.neighbours:
-            #print(f"Node {self.id} colour: {self.colour}")
-            #print(f"Node {neighbour.id} colour: {neighbour.get_colour()}")
+            # print(f"Node {self.id} colour: {self.colour}")
+            # print(f"Node {neighbour.id} colour: {neighbour.get_colour()}")
             if self.colour == neighbour.get_colour():
-                #print("Conflict detected! Checking personality")
-                #print(f"Node {self.id} type: {self.personality}, strength: {self.strength}")
+                # print("Conflict detected! Checking personality")
+                # print(f"Node {self.id} type: {self.personality}, strength: {self.strength}")
                 if self.personality == 0 and neighbour.personality == 0:
                     # aggressive vs aggressive
                     if self.strength > neighbour.strength:
                         neighbour.change_colour()
-                        
+
                     else:
                         self.change_colour()
                 elif (self.personality == 0 and neighbour.personality == 1):
@@ -91,15 +90,15 @@ class Node():
                     else:
                         self.change_colour()
                 elif self.personality == 1 and neighbour.personality == 0:
-                    self.change_colour()   
+                    self.change_colour()
             else:
                 pass
 
     def change_colour(self):
         """Change colour to a random colour in known list"""
-        #print(f"Number of colours: {len(self.known_colours)}")
+        # print(f"Number of colours: {len(self.known_colours)}")
         new_colour = random.choice(self.known_colours)
-        #print(f"Changing colour of node {self.id} from {self.colour} to {new_colour}", end='')
+        # print(f"Changing colour of node {self.id} from {self.colour} to {new_colour}", end='')
         self.set_colour(new_colour)
 
     def set_colour(self, colour: str):
@@ -137,10 +136,16 @@ class Node():
     def __hash__(self) -> int:
         return hash(self.id)
 
+    def __repr__(self):
+        return f"Node(id={self.id}, colour={self.colour}, personality={self.personality}, strength={self.personality_strength})"
+
+    def __str__(self):
+        return f"Node ID: {self.id}, Colour: {self.colour}, Personality: {'Aggressive' if self.personality == 0 else 'Passive'}, Strength: {self.personality_strength}"
+
 
 # helper functions
 
-def generate_graph(num_nodes: int = None, num_edges: int = None, seed: int = 0):
+def generate_graph(num_nodes: int = None, num_edges: int = None, p: int = 0.1):
     """
     Function to generate a graph using networkx.
 
@@ -164,9 +169,7 @@ def generate_graph(num_nodes: int = None, num_edges: int = None, seed: int = 0):
         G.add_edge(nodes[3], nodes[0])
     else:
         # small world graph
-        if seed is not None:
-            random.seed(seed)
-        G_small_world = nx.watts_strogatz_graph(num_nodes, num_edges, seed)
+        G_small_world = nx.watts_strogatz_graph(num_nodes, num_edges, p)
         nodes = [Node(str(i)) for i in range(num_nodes)]
         for node in nodes:
             G.add_node(node)
@@ -191,10 +194,8 @@ def node_fitmess(node: Node):
     conflicts = 0  # init counter
     for neighbour in node.get_neighbours():  # iterate through neighbours
         # check for collision of colours
-        #print(f"Node colour: {node.get_colour()}, Neighbour colour: {neighbour.get_colour()}")
         if node.get_colour() == neighbour.get_colour():
             conflicts += 1  # increment counter
-            #print(f"Conflict count: {conflicts}")
     return conflicts
 
 
@@ -211,7 +212,7 @@ def fitness_function(g: nx.Graph):
     return conflicts
 
 
-def init_nodes(g: nx.Graph, colours: list):
+def init_nodes(g: nx.Graph):
     """
     Function to give graph a random colouring from the given list of colours, 
     as well as updating each node with their neighbours.
@@ -220,11 +221,13 @@ def init_nodes(g: nx.Graph, colours: list):
     - g: nx.Graph - graph to be coloured
     - colours: list - list of colours to be used for the graph
     """
+    colours_list = np.random.choice(global_colours_list, min_colours)
+
     # assign random colours to nodes from the list of colours
     for node in g.nodes():
-        colour = random.choice(colours)
+        colour = random.choice(colours_list)
         node.set_colour(colour)
-        node.set_known_colours(colours)
+        node.set_known_colours(colours_list)
         # update neighbours
         # add all connected nodes to neighbours
         # Get actual node references from the graph
@@ -262,10 +265,14 @@ def algorithm(graph: nx.Graph, num_iterations: int):
         # update graph with new colours
         nx.draw(graph, pos, with_labels=True, labels=ids,
                 node_color=[node.get_colour() for node in graph.nodes()])
-        plt.pause(1)
+        if num_iterations <= 100:
+            plt.pause(0.05)
+        else:
+            plt.pause(0.025)
 
         # get fitness
         fitness = fitness_function(graph)
+        print(f"Iteration: {i}, Fitness: {fitness}")
         if fitness < best_fitness:
             best_fitness = fitness
             print("New best fitness:", best_fitness)
@@ -278,44 +285,28 @@ def algorithm(graph: nx.Graph, num_iterations: int):
         # loop through nodes
         # get them all to communicate with their neighbours
         for node in graph.nodes():
-            # print neighbours
-            print(f"Node {node.id} neighbours: {node.get_neighbours()}")
             node.communicate()
-
-        # plot the graph as well as the fitness on the same figure
-        # plt.plot(i, fitness, 'ro')
 
     return fitness_list
 
 
 if __name__ == '__main__':
-    """
-    Main function
-
-    Generates a graph, finds out minimum number of colours required for the graph and then runs the algorithm.
-
-    #### Procedure (idea)
-    1. Generate nodes
-    2. Generate graph using nodes
-    3. Find out min_colours for said graph
-    4. Make number available to graph
-    5. Run main algorithm (?) till either convergence or iterations reached
-    """
-    # trivial graph to demonstrate algorithm is functional
-    # graph = generate_graph(nodes, edges, seed)
-    graph = generate_graph(50, 20, 0)
+    graph = generate_graph(40, 6, 0.1)
     min_colours = minimum_colours(graph)
-    colours_list = np.random.choice(global_colours_list, min_colours)
     # initialise graph with colours
-    init_nodes(graph, colours_list)
+    init_nodes(graph)
 
-    # make empty plot to host updating graph
-    fig, ax = plt.subplots()
-    fitness = algorithm(graph, 300)
+    # make empty plot to host graph plot and fitness plot
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 1, 1)
+    fitness = algorithm(graph, 1000)
 
     # plot fitness over time and add to the same figure
-    plt.plot(fitness)
-    plt.xlabel('Iteration')
-    plt.ylabel('Fitness')
-    plt.title('Fitness over time')
+    # ensure no overlapping of plots
+    ax2 = fig.add_subplot(2, 1, 2)
+    ax2.plot(fitness)
+    ax2.set_title("Fitness over time")
+    ax2.set_xlabel("Iterations")
+    ax2.set_ylabel("Fitness")
+    ax2.grid(True)
     plt.show()
